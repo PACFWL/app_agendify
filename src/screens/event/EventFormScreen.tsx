4a2260c59d2fab0f4d4bdfdc2a185868069ff34c
PACFWL <estudanteprincipalgerali@gmail.com>
2025-05-22 21:48:51 -0300

    ♻️ Refact: "Adicionando estilo e cores para tema Dark IV"

--- src/screens/event/EventFormScreen.tsx
+++ src/screens/event/EventFormScreen.tsx
@@ -1,14 +1,15 @@
 import React, { useContext } from "react";
 import { Text, TextInput, Alert, ScrollView, TouchableOpacity, View } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import { AuthContext } from "../../contexts/AuthContext";
-import { createEvent } from "../../api/event";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { RootStackParamList } from "../../routes/Routes";
-import styles from "../../styles/EventFormScreenStyles";
 import { Picker } from '@react-native-picker/picker';
-import { useEventForm } from "../../hooks/useEventForm";
+
+import { AuthContext } from "../../contexts/AuthContext";
 import { ThemeContext } from "../../contexts/ThemeContext";
+import { createEvent } from "../../api/event";
+import { RootStackParamList } from "../../routes/Routes";
+import { useEventForm } from "../../hooks/useEventForm";
+import styles from "../../styles/EventFormScreenStyles";
 import { getColors } from "../../styles/themeColors";
 
 type Props = NativeStackScreenProps<RootStackParamList, "EventForm">;
@@ -16,17 +17,17 @@ type Props = NativeStackScreenProps<RootStackParamList, "EventForm">;
 const locationOptionsByFloor: Record<string, string[]> = {
   "0": ["Auditório", "Sala Maker", "Hall Principal", "Sala T01", "Sala T02", "Sala T03"],
   "1": ["Sala de Informática 001", "Sala de Informática 002", "Sala de Informática 003", "Sala de Informática 004", "Sala 111", "Sala de Informática 001", "Sala de Aula 001", "Sala de Aula 002", "Sala de Aula 003", "Sala de Aula 004", "Sala de Aula 005", "Sala de Aula 006", "Sala de Aula 007", "Sala de Aula 008", "Sala de Aula 009"],
-  "2": ["Sala de Informática 001", "Sala de Informática 002", "Sala de Informática 003", "Sala de Informática 004", "Sala de Aula 001", "Sala de Aula 002", "Sala de Aula 003", "Sala de Aula 004", "Sala de Aula 005", "Sala de Aula 006", "Sala de Aula 007", "Sala de Aula 008", "Sala de Aula 009", "Sala de Aula 010" ],
+  "2": ["Sala de Informática 001", "Sala de Informática 002", "Sala de Informática 003", "Sala de Informática 004", "Sala de Aula 001", "Sala de Aula 002", "Sala de Aula 003", "Sala de Aula 004", "Sala de Aula 005", "Sala de Aula 006", "Sala de Aula 007", "Sala de Aula 008", "Sala de Aula 009", "Sala de Aula 010"],
   "3": ["Sala de Aula 1", "Sala de Aula 2"],
 };
 
 const EventFormScreen = ({ navigation }: Props) => {
   const auth = useContext(AuthContext);
-const {  theme } = useContext(ThemeContext);
+  const { theme } = useContext(ThemeContext);
   const colors = getColors(theme);
 
   const {
-   handleAuthorChange,
+    handleAuthorChange,
     addAuthorField,
     removeAuthorField,
     handleCourseChange,
@@ -58,26 +59,22 @@ const {  theme } = useContext(ThemeContext);
     handleStartTimeChange,
     handleEndTimeChange,
     validateFields,
-    errors, 
+    errors,
     setErrors
   } = useEventForm();
 
-   const handleSubmit = async () => {
-   const validationErrors = validateFields();
+  const handleSubmit = async () => {
+    const validationErrors = validateFields();
     setErrors(validationErrors);
 
-     if (Object.keys(validationErrors).length > 0) {
-      return;
-    }
-    if (!auth?.user) return; 
+    if (Object.keys(validationErrors).length > 0) return;
+    if (!auth?.user) return;
 
     try {
-
       let cleanupDurationISO = "";
-      
+
       if (cleanupHours || cleanupMinutes) {
         cleanupDurationISO = "PT";
-
         if (cleanupHours) cleanupDurationISO += `${parseInt(cleanupHours)}H`;
         if (cleanupMinutes) cleanupDurationISO += `${parseInt(cleanupMinutes)}M`;
       }
@@ -91,25 +88,28 @@ const {  theme } = useContext(ThemeContext);
         relatedSubjects: eventData.relatedSubjects,
         authors: eventData.authors,
         courses: eventData.courses,
-        location: { name: eventData.locationName, floor: eventData.locationFloor },
+        location: {
+          name: eventData.locationName,
+          floor: eventData.locationFloor
+        },
         cleanupDuration: cleanupDurationISO || null,
       };
 
       const result = await createEvent(auth.user.token, formattedData);
-      
+
       if (!result.conflict) {
         Alert.alert("Sucesso", "Evento criado com sucesso!");
         navigation.goBack();
         return;
       }
-  
+
       const conflictingEvent = result.conflictData.existingEvent;
-    
-    navigation.navigate("ConflictResolution", {
-      newEvent: formattedData,
-      existingEvent: conflictingEvent,
-    });
-    
+
+      navigation.navigate("ConflictResolution", {
+        newEvent: formattedData,
+        existingEvent: conflictingEvent,
+      });
+
     } catch (error) {
       console.error(error);
       Alert.alert("Erro", "Erro ao criar evento.");
@@ -131,13 +131,13 @@ const {  theme } = useContext(ThemeContext);
           setErrors((prev) => ({ ...prev, name: "" }));
         }}
       />
-      {errors.name && <Text style={{ color: "red", marginBottom: 5 }}>{errors.name}</Text>}
+      {errors.name && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.name}</Text>}
 
       <Text style={[styles.label, { color: colors.text }]}>Data:</Text>
       <TouchableOpacity onPress={() => showPicker("date")}>
         <Text style={[styles.input, { color: colors.text }, { backgroundColor: colors.card, borderColor: colors.accent }]}>{eventData.day || "Selecionar data"}</Text>
       </TouchableOpacity>
-      {errors.day && <Text style={{ color: "red", marginBottom: 5 }}>{errors.day}</Text>}
+      {errors.day && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.day}</Text>}
       {showDatePicker && (
         <DateTimePicker
           mode="date"
@@ -151,7 +151,7 @@ const {  theme } = useContext(ThemeContext);
       <TouchableOpacity onPress={() => showPicker("start")}>
         <Text style={[styles.input, { color: colors.text }, { backgroundColor: colors.card, borderColor: colors.accent }]}>{eventData.startTime || "Selecionar horário"}</Text>
       </TouchableOpacity>
-      {errors.startTime && <Text style={{ color: "red", marginBottom: 5 }}>{errors.startTime}</Text>}
+      {errors.startTime && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.startTime}</Text>}
       {showStartTimePicker && (
         <DateTimePicker
           mode="time"
@@ -166,7 +166,7 @@ const {  theme } = useContext(ThemeContext);
       <TouchableOpacity onPress={() => showPicker("end")}>
         <Text style={[styles.input, { color: colors.text }, { backgroundColor: colors.card, borderColor: colors.accent }]}>{eventData.endTime || "Selecionar horário"}</Text>
       </TouchableOpacity>
-      {errors.endTime && <Text style={{ color: "red", marginBottom: 5 }}>{errors.endTime}</Text>}
+      {errors.endTime && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.endTime}</Text>}
       {showEndTimePicker && (
         <DateTimePicker
           mode="time"
@@ -187,7 +187,7 @@ const {  theme } = useContext(ThemeContext);
           setErrors((prev) => ({ ...prev, theme: "" }));
         }}
       />
-      {errors.theme && <Text style={{ color: "red", marginBottom: 5 }}>{errors.theme}</Text>}
+      {errors.theme && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.theme}</Text>}
 
       <Text style={[styles.label, { color: colors.text }]}>Público-alvo:</Text>
       <TextInput 
@@ -199,7 +199,7 @@ const {  theme } = useContext(ThemeContext);
           setErrors((prev) => ({ ...prev, targetAudience: "" }));
         }}
       />
-    {errors.targetAudience && <Text style={{ color: "red", marginBottom: 5 }}>{errors.targetAudience}</Text>}
+    {errors.targetAudience && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.targetAudience}</Text>}
 
       <Text style={[styles.label, { color: colors.text }]}>Modalidade:</Text>
        <View style={[styles.pickerContainer, { backgroundColor: colors.card, borderColor: colors.accent }]}>
@@ -214,7 +214,7 @@ const {  theme } = useContext(ThemeContext);
         <Picker.Item label="Híbrido" value="HIBRIDO" />
       </Picker>
       </View>
-      {errors.mode && <Text style={{ color: "red", marginBottom: 5 }}>{errors.mode}</Text>}
+      {errors.mode && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.mode}</Text>}
 
        <Text style={[styles.label, { color: colors.text }]}>Ambiente:</Text>
         
@@ -227,7 +227,7 @@ const {  theme } = useContext(ThemeContext);
           setErrors((prev) => ({ ...prev, environment: "" }));
         }}
       />
-      {errors.environment && <Text style={{ color: "red", marginBottom: 5 }}>{errors.environment}</Text>}
+      {errors.environment && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.environment}</Text>}
 
        <Text style={[styles.label, { color: colors.text }]}>Organizador:</Text>
         <TextInput 
@@ -239,7 +239,7 @@ const {  theme } = useContext(ThemeContext);
           setErrors((prev) => ({ ...prev, organizer: "" }));
         }}
       />
-      {errors.organizer && <Text style={{ color: "red", marginBottom: 5 }}>{errors.organizer}</Text>}
+      {errors.organizer && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.organizer}</Text>}
 
        <Text style={[styles.label, { color: colors.text }]}>Recursos:</Text>
         {eventData.resourcesDescription.map((resource, index) => (
@@ -261,7 +261,7 @@ const {  theme } = useContext(ThemeContext);
     <TouchableOpacity style={styles.addButton} onPress={addResourcesDescriptionField}>
       <Text style={styles.addButtonText}>Adicionar Recurso</Text>
     </TouchableOpacity>
-    {errors.resourcesDescription && <Text style={{ color: "red", marginBottom: 5 }}>{errors.resourcesDescription}</Text>}
+    {errors.resourcesDescription && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.resourcesDescription}</Text>}
 
        <Text style={[styles.label, { color: colors.text }]}>Forma de Divulgação:</Text>
         <TextInput 
@@ -273,7 +273,7 @@ const {  theme } = useContext(ThemeContext);
           setErrors((prev) => ({ ...prev, disclosureMethod: "" }));
         }}
       />
-      {errors.disclosureMethod && <Text style={{ color: "red", marginBottom: 5 }}>{errors.disclosureMethod}</Text>}
+      {errors.disclosureMethod && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.disclosureMethod}</Text>}
 
      <Text style={[styles.label, { color: colors.text }]}>Disciplinas Relacionadas:</Text>
     {eventData.relatedSubjects.map((relatedSubject, index) => (
@@ -295,7 +295,7 @@ const {  theme } = useContext(ThemeContext);
     <TouchableOpacity style={styles.addButton} onPress={addRelatedSubjectField}>
       <Text style={styles.addButtonText}>Adicionar Disciplina</Text>
     </TouchableOpacity>
-    {errors.relatedSubjects && <Text style={{ color: "red", marginBottom: 5 }}>{errors.relatedSubjects}</Text>}
+    {errors.relatedSubjects && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.relatedSubjects}</Text>}
 
        <Text style={[styles.label, { color: colors.text }]}>Estratégia de Ensino:</Text>
         <TextInput 
@@ -307,7 +307,7 @@ const {  theme } = useContext(ThemeContext);
           setErrors((prev) => ({ ...prev, teachingStrategy: "" }));
         }}
       />
-      {errors.teachingStrategy && <Text style={{ color: "red", marginBottom: 5 }}>{errors.teachingStrategy}</Text>}
+      {errors.teachingStrategy && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.teachingStrategy}</Text>}
 
        <Text style={[styles.label, { color: colors.text }]}>Autores:</Text>
 
@@ -332,7 +332,7 @@ const {  theme } = useContext(ThemeContext);
           <TouchableOpacity style={styles.addButton} onPress={addAuthorField}>
             <Text style={styles.addButtonText}>Adicionar Autor</Text>
           </TouchableOpacity> 
-      {errors.authors && <Text style={{ color: "red", marginBottom: 5 }}>{errors.authors}</Text>}
+      {errors.authors && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.authors}</Text>}
 
 
        <Text style={[styles.label, { color: colors.text }]}>Cursos:</Text>
@@ -356,7 +356,7 @@ const {  theme } = useContext(ThemeContext);
       <TouchableOpacity style={styles.addButton} onPress={addCourseField}>
         <Text style={styles.addButtonText}>Adicionar Curso</Text>
       </TouchableOpacity>
-      {errors.courses && <Text style={{ color: "red", marginBottom: 5 }}>{errors.courses}</Text>}
+      {errors.courses && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.courses}</Text>}
 
        <Text style={[styles.label, { color: colors.text }]}>Vínculo Disciplinar:</Text>
         <TextInput 
@@ -368,7 +368,7 @@ const {  theme } = useContext(ThemeContext);
           setErrors((prev) => ({ ...prev, disciplinaryLink: "" }));
         }}
       />
-      {errors.disciplinaryLink && <Text style={{ color: "red", marginBottom: 5 }}>{errors.disciplinaryLink}</Text>}
+      {errors.disciplinaryLink && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.disciplinaryLink}</Text>}
 
        <Text style={[styles.label, { color: colors.text }]}>Andar:</Text>
         <View style={[styles.pickerContainer, { backgroundColor: colors.card, borderColor: colors.accent }]}>
@@ -388,7 +388,7 @@ const {  theme } = useContext(ThemeContext);
         <Picker.Item label="3º Andar" value="3" />
       </Picker>
       </View>
-      {errors.locationFloor && <Text style={{ color: "red", marginBottom: 5 }}>{errors.locationFloor}</Text>}
+      {errors.locationFloor && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.locationFloor}</Text>}
 
        <Text style={[styles.label, { color: colors.text }]}>Local:</Text>
       <View style={[styles.pickerContainer, { backgroundColor: colors.card, borderColor: colors.accent }]}>
@@ -408,7 +408,7 @@ const {  theme } = useContext(ThemeContext);
         ))}
       </Picker>
       </View>
-      {errors.locationName && <Text style={{ color: "red", marginBottom: 5 }}>{errors.locationName}</Text>}
+      {errors.locationName && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.locationName}</Text>}
 
     <Text style={[styles.label, { color: colors.text }]}>Status do Evento:</Text>
     <View style={[styles.pickerContainer, { backgroundColor: colors.card, borderColor: colors.accent }]}>
@@ -423,7 +423,7 @@ const {  theme } = useContext(ThemeContext);
       <Picker.Item label="Planejado" value="PLANEJADO" />
     </Picker>
     </View>
-   {errors.status && <Text style={{ color: "red", marginBottom: 5 }}>{errors.status}</Text>}
+   {errors.status && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.status}</Text>}
        
        
        <Text style={[styles.label, { color: colors.text }]}>Status Administrativo:</Text>
@@ -445,7 +445,7 @@ const {  theme } = useContext(ThemeContext);
         <Picker.Item label="Indefinido" value="INDEFINIDO" />
       </Picker>
       </View>
-       {errors.administrativeStatus && <Text style={{ color: "red", marginBottom: 5 }}>{errors.administrativeStatus}</Text>}
+       {errors.administrativeStatus && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.administrativeStatus}</Text>}
         
            <Text style={[styles.label, { color: colors.text }]}>Prioridade:</Text>
            <View style={[styles.pickerContainer, { backgroundColor: colors.card, borderColor: colors.accent }]}>
@@ -465,8 +465,8 @@ const {  theme } = useContext(ThemeContext);
             <Picker.Item label="Crítica" value="CRITICA" />
           </Picker>
           </View>
-  {errors.priority && <Text style={{ color: "red", marginBottom: 5 }}>{errors.priority}</Text>}
-     
+  {errors.priority && <Text style={{ color: colors.error, marginBottom: 5 }}>{errors.priority}</Text>}
+
        <Text style={[styles.label, { color: colors.text }]}>Duração de Limpeza</Text>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <TextInput
--- src/styles/themeColors.ts
+++ src/styles/themeColors.ts
@@ -11,7 +11,7 @@ export const colors = {
     noEventText: "#888",
     text: "#000000", 
     accent: "#0288d1",
-    
+    error: "#d32f2f",
   },
   dark: {
     background: "#121212",
@@ -23,9 +23,8 @@ export const colors = {
     noEventText: "#aaa",
     text: "#ffffff",
     accent: "#4dd0e1", 
-    
+     error: "#ef5350",
   },
-
 };
 
 export const getColors = (theme: ThemeType) => colors[theme];
